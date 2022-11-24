84aba9469f3cc49a7266f0764826f16acad29c2e
KEN20960 <muchirinephat5@gmail.com>
2022-11-24 11:17:26 +0200

    Initialized new Reactive Spring project

---
+++ src/main/java/com/bpr/main/CrbNotificationsReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.bpr.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrbNotificationsReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrbNotificationsReactiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bpr/main/CrbNotificationsReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bpr.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrbNotificationsReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
